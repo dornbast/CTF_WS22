8d34b23ec20a816c7fec51346401cae91688620f
dornbast <80685574+dornbast@users.noreply.github.com>
2023-01-13 16:05:44 +0100

    Delete Challenge_1 directory

--- Challenge_1/deletefile_CTF_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ------------------------------------------------------------------------------------------------------------ # 
-# 					--- Delete FILE ---						       #
-# ------------------------------------------------------------------------------------------------------------ # 
-sudo umount /dev/loop0p1
-sudo losetup --detach /dev/loop0 
-sudo rm -r /home/ctf-admin/Desktop/CTF_Challenges/Challenge_1/CTF_Challenge_1.raw
-sudo rm -r /mnt/CTF
